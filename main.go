f461393654fcb81fa49f30624c66de0bde301909
gbrreel <gabrielggalloo@gmail.com>
2025-12-02 20:35:17 -0300

    Tranferencias

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type ContaCorrente struct {
-	titurlar      string
+	titular       string
 	numeroAgencia int
 	numeroConta   int
 	saldo         float64
@@ -19,15 +19,35 @@ func (c *ContaCorrente) Sacar(valorSaque float64) string {
 	}
 }
 
+func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
+	if valorDeposito < 0 {
+		c.saldo += valorDeposito
+		return "Valor de depósito inválido", c.saldo
+	} else {
+		c.saldo += valorDeposito
+		return "Depósito realizado com sucesso", c.saldo
+	}
+}
+
+func (c *ContaCorrente) ransferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorTransferencia < c.saldo {
+		c.saldo -= valorTransferencia
+		contaDestino.Depositar(valorTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
-	contaSilva := ContaCorrente{}
-	contaSilva.titurlar = "Silva"
-	contaSilva.saldo = 1000.0
+	contaSilva := ContaCorrente{titular: "Silva", saldo: 300}
+	contaGallo := ContaCorrente{titular: "Gallo", saldo: 500}
 
-	fmt.Println(contaSilva.saldo)
+	status := contaSilva.ransferir(200, &contaGallo)
 
-	fmt.Println(contaSilva.Sacar(-300))
+	fmt.Println(status)
 
-	fmt.Println(contaSilva.saldo)
+	fmt.Println(contaSilva)
+	fmt.Println(contaGallo)
 
 }
